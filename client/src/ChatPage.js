4f31be1d9ae6a114eef639fd19d2bef889808beb
vooora <126191857+vooora@users.noreply.github.com>
2024-07-01 17:50:14 +0530

    Adding depolyment link to chatpage

--- client/src/ChatPage.js
+++ client/src/ChatPage.js
@@ -30,7 +30,7 @@ function ChatPage() {
       setIsLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8000/chat", {
+        const response = await fetch("https://renee-chatbot.onrender.com/chat", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
